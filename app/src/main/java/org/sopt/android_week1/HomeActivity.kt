a09f9b2e296fb4043f9890f7c85837e8a7aae619
SSU\yb9854 <yb9854@naver.com>
2021-10-20 14:01:48 +0900

    add level2

--- app/src/main/java/org/sopt/android_week1/HomeActivity.kt
+++ app/src/main/java/org/sopt/android_week1/HomeActivity.kt
@@ -1,5 +1,7 @@
 package org.sopt.android_week1
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.android_week1.databinding.ActivityHomeBinding
@@ -10,7 +12,15 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        clickGithub()
+    }
+
+    private fun clickGithub() {
+        binding.ivGithub.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/yubinquitous"))
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/org/sopt/android_week1/SignInActivity.kt
+++ app/src/main/java/org/sopt/android_week1/SignInActivity.kt
@@ -1,42 +1,54 @@
 package org.sopt.android_week1
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import org.sopt.android_week1.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
 
+    private val signUpActivityLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val id = it.data?.getStringExtra("id")
+                val pw = it.data?.getStringExtra("pw")
+                binding.etId.setText(id)
+                binding.etPassword.setText(pw)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val id = binding.etId.text
-        val pw = binding.etPassword.text
+        clickLogin()
+        clickSignUp()
+    }
 
+    private fun clickLogin() {
         binding.btLogin.setOnClickListener {
-            if (id.toString().isEmpty() || pw.toString().isEmpty()) {
+            val id = binding.etId.text
+            val pw = binding.etPassword.text
+
+            if (id.isEmpty() || pw.isEmpty()) {
                 Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "${id}님 환영합니다", Toast.LENGTH_SHORT).show()
-                startHomeActivity()
+                val intent = Intent(this@SignInActivity, HomeActivity::class.java)
+                startActivity(intent)
             }
         }
+    }
+
+    private fun clickSignUp() {
         binding.btSignup.setOnClickListener {
-            startSignUpActivity()
+            val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+            signUpActivityLauncher.launch(intent)
         }
-        setContentView(binding.root)
-    }
-
-    private fun startSignUpActivity() {
-        val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun startHomeActivity() {
-        val intent = Intent(this@SignInActivity, HomeActivity::class.java)
-        startActivity(intent)
     }
 }
