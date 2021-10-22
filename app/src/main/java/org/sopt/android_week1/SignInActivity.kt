d1672753c3dcd5ff69870a8731faf8f9c09ebcdc
SSU\yb9854 <yb9854@naver.com>
2021-10-22 21:05:41 +0900

    add level1

--- app/src/main/java/org/sopt/android_week1/SignInActivity.kt
+++ app/src/main/java/org/sopt/android_week1/SignInActivity.kt
@@ -12,31 +12,31 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        clickLoginBtn()
+        clickSignUpBtn()
+    }
+
+    private fun clickLoginBtn() {
         val id = binding.etId.text
         val pw = binding.etPassword.text
 
         binding.btLogin.setOnClickListener {
-            if (id.toString().isEmpty() || pw.toString().isEmpty()) {
+            if (id.isEmpty() || pw.isEmpty()) {
                 Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "${id}님 환영합니다", Toast.LENGTH_SHORT).show()
-                startHomeActivity()
+                val intent = Intent(this@SignInActivity, HomeActivity::class.java)
+                startActivity(intent)
             }
         }
+    }
+
+    private fun clickSignUpBtn() {
         binding.btSignup.setOnClickListener {
-            startSignUpActivity()
+            val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+            startActivity(intent)
         }
-        setContentView(binding.root)
-    }
-
-    private fun startSignUpActivity() {
-        val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun startHomeActivity() {
-        val intent = Intent(this@SignInActivity, HomeActivity::class.java)
-        startActivity(intent)
     }
 }
