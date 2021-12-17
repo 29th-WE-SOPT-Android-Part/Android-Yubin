1011f067a42f54da21a41c36d1b0f3013cfa8396
SSU\yb9854 <yb9854@naver.com>
2021-12-17 17:04:26 +0900

    [ADD] week7 level2_1 done

--- app/src/main/java/org/sopt/android_week1/onboarding/OnBoardingFragment3.kt
+++ app/src/main/java/org/sopt/android_week1/onboarding/OnBoardingFragment3.kt
@@ -1,17 +1,22 @@
 package org.sopt.android_week1.onboarding
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
+import org.sopt.android_week1.R
 import org.sopt.android_week1.databinding.FragmentOnBoarding3Binding
 import org.sopt.android_week1.login.SignInActivity
 
 class OnBoardingFragment3 : Fragment() {
     private var _binding: FragmentOnBoarding3Binding? = null
     private val binding get() = _binding ?: error("Binding이 초기화되지 않았습니다.")
+    private lateinit var callback: OnBackPressedCallback
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +25,6 @@ class OnBoardingFragment3 : Fragment() {
         _binding = FragmentOnBoarding3Binding.inflate(layoutInflater, container, false)
 
         clickNextBtn()
-
         return binding.root
     }
 
@@ -30,4 +34,21 @@ class OnBoardingFragment3 : Fragment() {
             requireActivity().finish()
         }
     }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().navigate(R.id.action_onBoardingFragment3_to_onBoardingFragment1)
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callback.remove()
+    }
+
+
 }
